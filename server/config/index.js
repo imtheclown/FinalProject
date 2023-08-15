2d366db66f06d2d11c222f6e8f56337db4d9e469
Jonathan Calugas <jccalugas@up.edu.ph>
2023-08-15 12:10:17 +0800

    edited the package.json

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   port: 5000,
-  dbUri: 'mongodb://127.0.0.1:27017/FinalProject',
+  dbUri:
+    'mongodb+srv://jccalugas:Hello-World14@practicecluster.fvlgq6r.mongodb.net/FinalProject?retryWrites=true&w=majority',
   dbCredentials: { user: '', pass: '' },
   apiKey: 'Some Config Here'
 };
